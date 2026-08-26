de51eb64629d2f22f217ea711d34e090d51b314e
Brad Schmitz <dolphinswarmentertainment@gmail.com>
2026-08-26 12:04:01 -0600

    Setting up GitHub pages scaffolding

---
+++ shared/back-button.js
@@ -0,0 +1,8 @@
+class BackButton extends HTMLElement {
+	connectedCallback() {
+		const href = this.getAttribute('href') || '../index.html';
+		this.innerHTML = `<a class="back-button" href="${href}">&larr; Back</a>`;
+	}
+}
+
+customElements.define('back-button', BackButton);
